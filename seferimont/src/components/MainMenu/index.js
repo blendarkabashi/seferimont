1599b65780d751a4e3cd787855f5041ccc6fcf53
blendar-opgny <blendar@opgny.com>
2024-03-06 09:02:21 +0100

    change label

--- seferimont/src/components/MainMenu/index.js
+++ seferimont/src/components/MainMenu/index.js
@@ -74,9 +74,10 @@ const MainMenu = ({ setSidebarOpen }) => {
                 src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                 alt=""
               />
+
               <span className="hidden lg:flex lg:items-center">
                 <span className="ml-4 text-sm font-semibold leading-6 text-gray-900" aria-hidden="true">
-                  user
+                  User
                 </span>
                 <ChevronDownIcon className="ml-2 h-5 w-5 text-gray-400" aria-hidden="true" />
               </span>
